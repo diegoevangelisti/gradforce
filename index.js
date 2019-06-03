89832697ab68f94a0ee33a985ff7f27cc93a10fa
Diego <evangelistidiego@gmail.com>
2019-06-04 10:18:30 +1200

    mlab credentials changed

--- index.js
+++ index.js
@@ -274,7 +274,7 @@ app.listen(process.env.PORT || 5000, async function () {
 });
 
 //MLAB HEROKU
-mongoose.connect("mongodb://backend:"+proccess.env.MLAB_PASSWORD+"@ds259596.mlab.com:59596/heroku_xk93l586", {useNewUrlParser: true})
+mongoose.connect("mongodb://backend:"+process.env.MLAB_PASSWORD+"@ds259596.mlab.com:59596/heroku_xk93l586", {useNewUrlParser: true})
 
 //LOCAL HOSTING
 // mongoose.connect("mongodb://localhost/gradforce-local", {
