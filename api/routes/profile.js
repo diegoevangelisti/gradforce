b9d5e1d5be4279a540df1ccfc0ed87fdbfade1ac
Diego <evangelistidiego@gmail.com>
2019-06-07 16:27:24 +1200

    css and header changes

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -1,9 +1,15 @@
 var express = require("express");
 const router = express.Router();
 
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+      return next();
+  }
+  res.redirect("./auth/login");
+}
 
-router.get("/", (req, res) => {
-    res.render("../views/profile", {user: req.user});
+router.get("/", isLoggedIn, (req, res) => {
+    res.render("../views/studentprofile", {user: req.user, isLoggedIn: true});
     console.log("User: "+req.user)
   });
 module.exports = router;
