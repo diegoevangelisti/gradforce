15679286fd4c3b5651988059b22bac33d361ae4d
Diego <evangelistidiego@gmail.com>
2019-06-09 22:54:20 +1200

    page upload changes

--- index.js
+++ index.js
@@ -233,10 +233,10 @@ app.listen(process.env.PORT || 5000, async function () {
 mongoose.connect("mongodb://backend:"+process.env.MLAB_PASSWORD+"@ds259596.mlab.com:59596/heroku_xk93l586", {useNewUrlParser: true})
 
 //LOCAL HOSTING
-//  mongoose.connect("mongodb://localhost/gradforce-local", {
-//    useNewUrlParser: true,
-//    useFindAndModify: false
-//  });
+ /* mongoose.connect("mongodb://localhost/gradforce-local", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  });*/
 
 app.use((req, res, next) => {
   const error = new Error('Not found');
