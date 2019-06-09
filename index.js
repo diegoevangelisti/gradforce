0237266fcbc6e46cbcbadb811c36b0195ab3595b
Diego <evangelistidiego@gmail.com>
2019-06-09 22:26:37 +1200

    mlab on

--- index.js
+++ index.js
@@ -230,13 +230,13 @@ app.listen(process.env.PORT || 5000, async function () {
 });
 
 //MLAB HEROKU
-//mongoose.connect("mongodb://backend:"+process.env.MLAB_PASSWORD+"@ds259596.mlab.com:59596/heroku_xk93l586", {useNewUrlParser: true})
+mongoose.connect("mongodb://backend:"+process.env.MLAB_PASSWORD+"@ds259596.mlab.com:59596/heroku_xk93l586", {useNewUrlParser: true})
 
 //LOCAL HOSTING
- mongoose.connect("mongodb://localhost/gradforce-local", {
-   useNewUrlParser: true,
-   useFindAndModify: false
- });
+//  mongoose.connect("mongodb://localhost/gradforce-local", {
+//    useNewUrlParser: true,
+//    useFindAndModify: false
+//  });
 
 app.use((req, res, next) => {
   const error = new Error('Not found');
