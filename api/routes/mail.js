23de7147070d730cdb3569f5eac6437329175558
Diego <evangelistidiego@gmail.com>
2019-06-11 13:22:59 +1200

    status changed after mail

--- api/routes/mail.js
+++ api/routes/mail.js
@@ -18,38 +18,65 @@ router.post("/send-email/:id", (req, res) => {
     let subject = req.body.subject;
     let content = req.body.content;
     let email = req.body.email;
-
+    let type = req.body.type;
     let id = req.params.id;
+
     User.findById(id).then((user) => {
 
+            //change status and description of user
+            //according to admin's decision
 
-        async function main() {
+            if (type == "interview") {
 
-            var transporter = nodemailer.createTransport({
-                host: 'smtp.gmail.com',
-                port: 465,
-                secure: true, // use SSL
-                auth: {
-                    user: 'gradforce.co.nz@gmail.com',
-                    pass: process.env.GMAIL
-                }
+                user.status = "Interview mail sent";
+                user.description = "Interview in process";
+                user.save();
+
+            } else if (type == "upskilling") {
+
+                user.status = "Upskilling mail sent";
+                user.description = "Upskilling in process";
+                user.save();
+
+            }
+
+            //send mail to user
+
+            async function main() {
+
+                var transporter = nodemailer.createTransport({
+                    host: 'smtp.gmail.com',
+                    port: 465,
+                    secure: true, // use SSL
+                    auth: {
+                        user: 'gradforce.co.nz@gmail.com',
+                        pass: process.env.GMAIL
+                    }
+                });
+
+                let info = await transporter.sendMail({
+                    from: '"GradForce" <gradforce.co.nz@gmail.com>',
+                    to: email,
+                    subject: subject,
+                    text: content,
+                    // html: "<b>Hello world?</b>" // html body
+                });
+
+                console.log("Message sent: %s", info.messageId);
+                console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+            }
+
+            main().catch(console.error);
+            res.redirect("/adminpanel/tables");
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
             });
+        });
 
-            let info = await transporter.sendMail({
-                from: '"GradForce" <gradforce.co.nz@gmail.com>',
-                to: email,
-                subject: subject,
-                text: content,
-                // html: "<b>Hello world?</b>" // html body
-            });
 
-            console.log("Message sent: %s", info.messageId);
-            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-        }
-
-        main().catch(console.error);
-        res.redirect("/adminpanel/tables");
-    })
 });
 
 module.exports = router;
